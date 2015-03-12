00c3f94b14f5f9141c93899e0e9ffa0ff592639f
xh <xionghan00@126.com>
2015-03-12 20:32:06 +0800

    +

--- script/db.sql
+++ script/db.sql
@@ -30,7 +30,7 @@ CREATE TABLE `t_request` (
   `create_time` datetime NOT NULL COMMENT '创建时间',
 
   `last_update_time` datetime NOT NULL DEFAULT "0000-00-00 00:00:00" COMMENT '最后一次状态更新的时间',
-
+  `last_operator_id` varchar(15) NOT NULL DEFAULT "" COMMENT '最后操作者',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -43,7 +43,7 @@ CREATE TABLE `t_request_op_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(15) NOT NULL COMMENT '是谁操作的',
   `request_id` int(11) NOT NULL COMMENT '操作的那一个request',
-  `op_type` int(1) NOT NULL DEFAULT 0 COMMENT '操作是什么 1:拒绝 2:同意',
+  `op_type` int(1) NOT NULL DEFAULT 0 COMMENT '操作是什么 0:创建 1:拒绝 2:同意',
   `create_time` datetime NOT NULL DEFAULT "0000-00-00 00:00:00" COMMENT '操作产生时间',
   `remark` varchar(512) NOT NULL DEFAULT "" COMMENT '备注',
   PRIMARY KEY (id)
